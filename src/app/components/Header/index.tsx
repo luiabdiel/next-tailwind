ddb1bfc04be4a85419fcfce14165fc8999927a8f
luiabdiel <luiggiabdieldev@gmail.com>
2024-09-22 18:27:16 -0300

    responsive header

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -31,9 +31,9 @@ export function Header() {
     border-b border-b-gray-100 flex items-center"
     >
       <GridContainer className="flex items-center justify-between">
-        <div className="flex items-center gap-10">
+        <div className="w-full xl:w-auto flex items-center justify-between gap-10">
           <Image src="/logo.svg" width={142} height={32} alt="Logo" />
-          <nav className="flex items-center gap-8">
+          <nav className="hidden xl:flex items-center gap-8">
             {menuItems.map(({ url, title, hasDropdown }, index) => (
               <ItemMenu
                 key={index}
@@ -43,9 +43,24 @@ export function Header() {
               />
             ))}
           </nav>
+          <button>
+            <Image
+              src="/hamburger.svg"
+              width={40}
+              height={40}
+              alt="Menu Hamburger"
+              className="block xl:hidden"
+            />
+          </button>
         </div>
         <div>
-          <Image src="/avatar.svg" width={40} height={40} alt="woman" />
+          <Image
+            src="/avatar.svg"
+            width={40}
+            height={40}
+            alt="woman"
+            className="hidden xl:block"
+          />
         </div>
       </GridContainer>
     </header>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,12 @@
 import { Fragment } from "react";
-import {
-  Header,
-  SectionCompanies,
-  SectionFeatures,
-  SectionHero,
-} from "./components";
+// import { SectionCompanies, SectionFeatures, SectionHero } from "./components";
 
 export default function Home() {
   return (
     <Fragment>
-      <Header />
-      <SectionHero />
+      {/* <SectionHero />
       <SectionCompanies />
-      <SectionFeatures />
+      <SectionFeatures /> */}
     </Fragment>
   );
 }
